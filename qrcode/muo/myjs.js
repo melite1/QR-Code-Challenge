9062507c4dafab04a45174ae8f2932df61144cf7
melite1 <117902730+melite1@users.noreply.github.com>
2023-05-30 15:26:00 +0100

    An Attempt on my first challenge
    This QR Code challenge is the first of many from Frontend Mentor

---
+++ qrcode/muo/myjs.js
@@ -0,0 +1,9 @@
+let n = "big woman";
+
+document.getElementById("nne").innerHTML = "Chimma is a " + n;
+
+document.write();
+
+const car = {name: ["volvo", "jaguar", "pampam", "caterpiller"], color: ["black", "blue", "white", "green", "purple"], price:[200, 500, 1000, 1500, 2000] };
+document.getElementById("nna").innerHTML = "the name of my car is " + car.name[0] + ". it is " + car.color[2] + " and sold for " + car.price[3];  
+// document.write(car);
